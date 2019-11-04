cb1aa17aed029bba4fbf736eaac5a6e139e5d33d
Anish <anishanne1@gmail.com>
2019-11-04 18:43:25 -0500

    :pencil2: Made Code::Together lowercase.

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports = {
             .setAuthor(msg.author.username,msg.author.avatarURL)
             .addField('**__Commands__**','!add-bot {Bots id} {prefix}\n!tag [tag]\n!paste {code}',true)
             .addField('**__Info:__**','Can only be used with the Certified role\nUseful instead of typing tags out\nPlease use if the code is bigger than it should be',true)
-            .addField('**__About__**','Made for the Code::Together server!');
+            .addField('**__About__**','Made for the code::together server!');
         msg.channel.send(embed);
     },
 };
