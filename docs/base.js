137bd26a9ab9f41a15ae04f1264cceda0606283a
flammableassassin <43076116+flamableassassin@users.noreply.github.com>
2019-11-04 12:53:32 +0000

    :tada: Added base for commands

---
+++ docs/base.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'help', //how it is called
+    description: 'description', // the description.. 
+    args: false, // if arguments are needed
+    deletemsg: false, //if the bot will delete the command msg
+    response:'', //the response if no arguments
+    execute(msg, args,client) {
+      //Code_Goes_Here
+    },
+};
