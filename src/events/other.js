3b551cfcf79b33a6d69ccbc5ce2c1fb41dbcb55d
flamableassassin <flamableassassin@users.noreply.github.com>
2020-05-30 22:08:42 +0100

    :ok_hand: idk why i tried

--- src/events/other.js
+++ src/events/other.js
@@ -1,14 +1,6 @@
 module.exports = function(client, config) {
     client.on("ready", async () => {
         console.log("Bot logged in!");
-
-        if (!config.dev_mode) {
-            const guildAdded = client.guilds.get(config.servers.main.id);
-            const memberCount = guildAdded.members.filter(member => !member.user.bot).length;
-            await guildAdded.channels.get(config.servers.main.dev_count_channel).edit({
-                name: `Devs: ${memberCount}`
-            });
-        }
     })
     process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
 
