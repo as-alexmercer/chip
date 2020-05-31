05bf80b3481f4178e5faf5921eec65330868718b
flamableassassin <flamableassassin@users.noreply.github.com>
2020-05-31 23:42:37 +0100

    :ok_hand: changing how disabled events are done

--- bot.js
+++ bot.js
@@ -1,14 +1,36 @@
 require('dotenv').config()
 const {
     loaders,
-    disabled_events,
     events
 } = require("./src/index.js")
 const config = require('./config.js');
 
 const eris = require('eris', {
     disableEveryone: true,
-    disableEvents: disabled_events(),
+    disableEvents: {
+        "CHANNEL_CREATE": true,
+        "CHANNEL_UPDATE": true,
+        "CHANNEL_DELETE": true,
+        "GUILD_BAN_ADD": true,
+        "GUILD_BAN_REMOVE": true,
+        "GUILD_CREATE": true,
+        "GUILD_DELETE": true,
+        "GUILD_MEMBER_ADD": false,
+        "GUILD_MEMBER_REMOVE": true,
+        "GUILD_MEMBER_UPDATE": false,
+        "GUILD_ROLE_CREATE": true,
+        "GUILD_ROLE_DELETE": true,
+        "GUILD_ROLE_UPDATE": true,
+        "GUILD_UPDATE": true,
+        "MESSAGE_CREATE": false,
+        "MESSAGE_DELETE": true,
+        "MESSAGE_DELETE_BULK": true,
+        "MESSAGE_UPDATE": true,
+        "PRESENCE_UPDATE": true,
+        "TYPING_START": true,
+        "USER_UPDATE": true,
+        "VOICE_STATE_UPDATE": true
+    },
     maxShards: 1
 });
 
