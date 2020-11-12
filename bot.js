c42d32877d7a805047d9ae6210854809a18d6b3d
flamableassassin <flamableassassin@users.noreply.github.com>
2020-11-12 21:49:12 +0000

    :poop: adding Intents

--- bot.js
+++ bot.js
@@ -4,31 +4,19 @@ const config = require('./config.js');
 
 const eris = require('eris', {
     disableEveryone: true,
-    disableEvents: {
-        "CHANNEL_CREATE": true,
-        "CHANNEL_UPDATE": true,
-        "CHANNEL_DELETE": true,
-        "GUILD_BAN_ADD": true,
-        "GUILD_BAN_REMOVE": true,
-        "GUILD_CREATE": true,
-        "GUILD_DELETE": true,
-        "GUILD_MEMBER_ADD": false,
-        "GUILD_MEMBER_REMOVE": true,
-        "GUILD_MEMBER_UPDATE": false,
-        "GUILD_ROLE_CREATE": true,
-        "GUILD_ROLE_DELETE": true,
-        "GUILD_ROLE_UPDATE": true,
-        "GUILD_UPDATE": true,
-        "MESSAGE_CREATE": false,
-        "MESSAGE_DELETE": true,
-        "MESSAGE_DELETE_BULK": true,
-        "MESSAGE_UPDATE": true,
-        "PRESENCE_UPDATE": true,
-        "TYPING_START": true,
-        "USER_UPDATE": true,
-        "VOICE_STATE_UPDATE": true
-    },
-    maxShards: 1
+    maxShards: 1,
+    intents: 514
+    // -READY
+    // - RESUMED
+    // - VOICE_SERVER_UPDATE
+    // - USER_UPDATE
+    // - GUILD_MEMBER_ADD
+    // - GUILD_MEMBER_UPDATE
+    // - GUILD_MEMBER_REMOVE
+    // - MESSAGE_CREATE
+    // - MESSAGE_UPDATE
+    // - MESSAGE_DELETE
+    // - MESSAGE_DELETE_BULK
 });
 
 var token = config.dev_mode ? config.tokens.bot.dev : config.tokens.bot.main
